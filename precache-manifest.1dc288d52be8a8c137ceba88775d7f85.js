47efbc5c1f9596a6b7ef6bfc2daaa9f8f822d75d
algerieno <aymen.algerieno2014@gmail.com>
2020-09-18 11:27:29 +0100

    Updates

---
+++ precache-manifest.1dc288d52be8a8c137ceba88775d7f85.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b288f520de7b15b1114bc76da3d292c",
+    "url": "/spotify-clone/index.html"
+  },
+  {
+    "revision": "eeb1a62bcb5dd5e8334b",
+    "url": "/spotify-clone/static/css/main.bf57b2f7.chunk.css"
+  },
+  {
+    "revision": "2c8672e2357afc96af4d",
+    "url": "/spotify-clone/static/js/2.f428ba9c.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/spotify-clone/static/js/2.f428ba9c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eeb1a62bcb5dd5e8334b",
+    "url": "/spotify-clone/static/js/main.37e3aef2.chunk.js"
+  },
+  {
+    "revision": "6a384b9ba3b95b4a0f87",
+    "url": "/spotify-clone/static/js/runtime-main.7e6ac335.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/spotify-clone/static/media/logo.5d5d9eef.svg"
+  }
+]);
